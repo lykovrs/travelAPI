aca8a8d57e6a523d6070154267e7f2211c30a6bc
lykovrs <lykovrs@gmail.com>
2015-09-02 11:01:39 +0300

    add documentation and refactoring

---
+++ cards.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Роман on 01.09.2015.
+ */
+var cards = [
+    {
+        "name": "Gerona Airport",
+        "type": "aircraft",
+        "number": "SK455",
+        "from": "Barcelona",
+        "to": "Stockholm",
+        "seat": "3A",
+        "gate": "45B",
+        "baggage": "Baggage drop at ticket counter 344"
+    },
+    {
+        "name": "Madrid",
+        "type": "train",
+        "number": "78A",
+        "from": null,
+        "to": "Barcelona",
+        "seat": "45B"
+    },
+    {
+        "name": "Stockholm",
+        "type": "aircraft",
+        "number": "SK22",
+        "from": "Gerona Airport",
+        "to": "New York JFK",
+        "seat": "7B",
+        "gate": "22",
+        "baggage": "Baggage will be automatically transferred from your last leg"
+    },
+    {
+        "name": "New York JFK",
+        "from": "Stockholm",
+        "to": null
+    },
+    {
+        "name": "Barcelona",
+        "type": "bus",
+        "number": "78A",
+        "from": "Madrid",
+        "to": "Gerona Airport",
+        "seat": "No seat assignment"
+    }
+
+];
